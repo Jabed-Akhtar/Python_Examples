c291c9c2e9c727fd4e56f7ad6467b53157584b23
Jabed-Akhtar <jabed.akhtar@outlook.com>
2022-11-03 20:55:35 +0100

    some old works added

---
+++ py_ws/projects/Chef_cook/main_file.py
@@ -0,0 +1,13 @@
+from Chef import Chef
+from chineseChef import ChineseChef
+
+# my chef
+myChef = Chef()
+myChef.make_special_dish()
+
+# my chinese chef
+mychineseChef = ChineseChef()
+mychineseChef.make_special_dish()
+
+
+
